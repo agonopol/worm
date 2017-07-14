edc78eaec1df4d234aee06f37cbee87d6b6127e7
Alex <agonopol@gmail.com>
2017-07-14 12:01:40 +0200

    [ag] minor plot changes

--- lib/cluster.m
+++ lib/cluster.m
@@ -1,15 +1,16 @@
-function cluster(adj, neurons, options)
+function contracted = cluster(adj, neurons, options)
 
     markov = rownorm(adj);
     [V,D] = eig(markov);
     matrix = bsxfun(@times,V(:,1:50)',diag(D(1:50, :)))';
     
-    
+
     options.sizefn = @(clusters, labels) centrality(adj, clusters)';
     options.labelfn = @(clusters, labels) label(labels, centrality(adj, clusters)');
+    options.plotfn = @plotworm;
 
     contractor = ContractionClustering(matrix, cellstr(neurons), options);
-    contractor = contractor.contract();
+    contracted = contractor.contract();
     
     % Write out cluster assigments
     
@@ -17,8 +18,8 @@ function cluster(adj, neurons, options)
     fprintf(f, join(string(neurons'), ','));
     fprintf(f, '\n');
 
-    for i = 1:min(size(contractor.clusterAssignments))
-        fprintf(f, join(string(contractor.clusterAssignments(i,:)), ','));
+    for i = 1:min(size(contracted.clusterAssignments))
+        fprintf(f, join(string(contracted.clusterAssignments(i,:)), ','));
         fprintf(f, '\n');
     end
     fclose(f);
@@ -30,15 +31,17 @@ function cluster(adj, neurons, options)
     fprintf(f, '\n');
     
     
-    for i = 1:min(size(contractor.clusterAssignments))
-        fprintf(f, join(string(centrality(adj, contractor.clusterAssignments(i,:))), ','));
+    for i = 1:min(size(contracted.clusterAssignments))
+        fprintf(f, join(string(centrality(adj, contracted.clusterAssignments(i,:))), ','));
         fprintf(f, '\n');
     end        
     fclose(f);
 
     % Write out sanky for the last 4 iterations
     for i = 0:4
-        target = strcat(options.destination, 'step-', string(contractor.iteration - i), '-sanky.html');
-        sanky(contractor.clusterAssignments(1:end-i, :), neurons, target);
+        target = strcat(options.destination, 'step-', string(contracted.iteration - i), '-sanky.html');
+        sanky(contracted.clusterAssignments(1:end-i, :), neurons, target);
     end
+    
+    
 end
